44a14ea249280e979e94266cb79df8b0103cc84f
Gabe Kangas <gabek@real-ity.com>
2023-03-11 16:09:41 -0800

    Update deploy job.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,16 +41,9 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/gabek/fedidocs/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.dev/gabek/fedidocs/tree/main/",
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/gabek/fedidocs/tree/main/packages/create-docusaurus/templates/shared/",
-        },
+
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
