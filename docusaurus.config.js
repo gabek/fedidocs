ef6b4a9385f1766b975d419b8b2d291ee41feadc
Todd Brown <todd.brown@hey.com>
2023-06-20 10:42:01 +0000

    respect prefers color scheme

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -58,6 +58,9 @@ const config = {
     ({
       // Replace with your project's social card
       image: "img/docusaurus-social-card.jpg",
+      colorMode: {
+        respectPrefersColorScheme: true,
+      },
       navbar: {
         title: "Fedi Devs",
         logo: {
